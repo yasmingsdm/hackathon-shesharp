234a83e241e1eb2536ea89dd2564a0b1fdb7f7e5
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-06-10 10:50:49 +0200

    job component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+
+
 import Sidebar from './components/Sidebar';
 
 function App() {
--- src/service/JobAPI.js
+++ src/service/JobAPI.js
@@ -2,9 +2,8 @@ import axios from 'axios'
 
 export const getAllJobs = async()=>{
         const response = await axios.get('https://sourcestack-api.com/jobs?name=frontend&exact=false', {
-            headers: {'x-api-key': '',
+            headers: {'x-api-key': 'SECRET_KEY HERE',
                     'Content-Type': 'application/json'},
           })
         return response.data.data
 }
-
