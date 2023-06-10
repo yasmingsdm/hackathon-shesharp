f3871ea441dffa857a4bb3a2c382d42f3ce2cd2b
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-06-10 11:38:58 +0200

    finish

--- src/service/JobAPI.js
+++ src/service/JobAPI.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 export const getAllJobs = async()=>{
-        const response = await axios.get('https://sourcestack-api.com/jobs?name=frontend&exact=false', {
-            headers: {'x-api-key': 'SECRET_KEY HERE',
-                    'Content-Type': 'application/json'},
-          })
+        // const response = await axios.get('https://sourcestack-api.com/jobs?name=frontend&exact=false', {
+        //     headers: {'x-api-key': 'SECRET_KEY HERE',
+        //             'Content-Type': 'application/json'},
+        //   })
+        const response = await axios.get('https://shesharpnl.github.io/hackathon-2023.sourcestack-data/assets/sourcestack-data-global.json')
         return response.data.data
 }
